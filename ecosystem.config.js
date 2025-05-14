c76444febdde654be935d3d2799fbd2362befff8
Brian Gates <brian.g.gates@gmail.com>
2025-05-14 14:57:50 -0700

    add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "relative-time",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 4000",
+      cwd: "./", // Set this to the absolute path on your server
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 4000,
+      },
+    },
+  ],
+};
