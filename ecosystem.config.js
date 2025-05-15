347e032ded6c476d5681ae4661a51389abf33c9a
Brian Gates <brian.g.gates@gmail.com>
2025-05-14 18:21:03 -0700

    fix port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
+const PORT = 3002;
+
 module.exports = {
   apps: [
     {
       name: "relative-time",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 4000",
+      args: `start -p ${PORT}`,
       cwd: "./", // Set this to the absolute path on your server
       instances: 1,
       autorestart: true,
@@ -11,7 +13,7 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
-        PORT: 3002,
+        PORT,
       },
     },
   ],
